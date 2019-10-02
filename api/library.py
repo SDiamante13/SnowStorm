1d46c583c54e9dad6f55ff264974e3c5fea55c7e
Steven Diamante <steven@pivotal.io>
2019-10-01 22:28:56 -0500

    WIP

--- api/library.py
+++ api/library.py
@@ -1,5 +1,5 @@
-from flask import Response, current_app, make_response, Blueprint, jsonify
-
+from flask import Response, current_app, make_response, Blueprint, jsonify, request, session
+from .stream import StreamService
 
 STREAM = Blueprint('stream', __name__, url_prefix='/stream')
 
@@ -20,3 +20,14 @@ def get_playing():
     title = current_app.config.get('stream').title.value.decode()
     artist = current_app.config.get('stream').artist.value.decode()
     return make_response(jsonify({'title': title, 'artist': artist}), 200)
+
+@STREAM.route('/queue', methods = ['GET', 'POST'])
+def queueSongs():
+   if request.method == 'GET':
+       return make_response(jsonify({'title': current_app.config.get('stream').song_queue.get()}, 200))
+   elif request.method == 'POST':
+       title = request.args.get('title', '')
+       artist = request.args.get('artist', '')
+       current_app.config.get('stream').add_song_to_queue(title)
+       queue = current_app.config.get('stream').song_queue
+       return make_response(jsonify({'title': title, 'artist': artist}), 200)   
